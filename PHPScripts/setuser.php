ae2b64f61f9346fffe1a6f10d207b25fdf59b86d
Danilo Santagata <d.santagata@glarotech.ch>
2020-10-13 16:47:03 +0200

    Log event of REST API by User

--- PHPScripts/setuser.php
+++ PHPScripts/setuser.php
@@ -11,34 +11,36 @@ header('Content-type: application/json');
  * Konstanten und andere Variablen
  */
 $response = array();
-$method = "";
+$method   = "";
 
 /**
  * Defines
  */
-define('HOST','localhost');
-define('USER','proapp');
-define('PASS','**********');
-define('DB','**********');
+define('HOST', 'localhost');
+define('USER', 'proapp');
+define('PASS', '**********');
+define('DB', '**********');
 
 /**
  * Hier wird entschieden, was fuer eine Aktion mit dem User gemacht werden soll
  * bsp. addNewUser, isUserinDB, getUser etc.
  */
-if(isset($_POST["method"])){
+if (isset($_POST["method"])) {
     $method = $_POST['method'];
-}else{
+}
+else {
     echo json_encode(createFailResponse('Fail'));
 }
 
 // Switch entscheidet, was für eine Aktion gestartet werden soll
 // Switch entscheidet, was für eine Aktion gestartet werden soll..
-switch ($method){
+switch ($method) {
     //Wenn sich ein Benutzer Anmelden will und schon ein login besitzt "user> hol, pwd> 12345"
     case 'loginuser':
-        validate($_POST['kuerzel'],$_POST['pwd']);
+        validate($_POST['kuerzel'], $_POST['pwd']);
         break;
     case 'adduser':
+        logger::log('adduser');
         break;
     default:
         echo json_encode(createFailResponse('MethodNotFoundException'));
@@ -48,36 +50,37 @@ switch ($method){
 /*
 * Function validiert den Username und das Password. sehr sehr schlecht...
 */
-function validate($kurz, $pwd){
-   $con = mysqli_connect(HOST,USER,PASS,DB);
-   // In so vielen Schichten falsch. Passwörter müssen gehasht sein. So nicht.
-    $sql = "select * from schmidt_user where token='".$kurz."' and pwd='".$pwd."'";
+function validate($kurz, $pwd) {
+    $con = mysqli_connect(HOST, USER, PASS, DB);
+    // In so vielen Schichten falsch. Passwörter müssen gehasht sein. So nicht.
+    $sql = "select * from schmidt_user where token='" . $kurz . "' and pwd='" . $pwd . "'";
     //$sql = "select * from schmidt_user";
-    $res = mysqli_query($con,$sql);
+    $res    = mysqli_query($con, $sql);
     $result = array();
 
-    while($row = mysqli_fetch_row($res)){
+    while ($row = mysqli_fetch_row($res)) {
         array_push($result,
-            array('userid'=> $row[0],
-                'name' => $row[1],
-                'lastname' => $row[2],
-                'token' => $row[3],
-                'pwd' => $row[4],
-                'vertrag' => $row[5]));
+            array('userid'   => $row[0],
+                  'name'     => $row[1],
+                  'lastname' => $row[2],
+                  'token'    => $row[3],
+                  'pwd'      => $row[4],
+                  'vertrag'  => $row[5]));
     }
-
-    echo json_encode(array("result"=>$result));
+    logger::log('validate user');
+    echo json_encode(array("result" => $result));
     mysqli_close($con);
 
- }
+}
 
 /*
 * Function create an Success-Array
 */
-function createSuccessRespone($message, $value){
-    $response['code'] = 200;
+function createSuccessRespone($message, $value) {
+    $response['code']   = 200;
     $response['status'] = $message;
-    $response['value'] = $value;
+    $response['value']  = $value;
+    logger::log($message);
     return $response;
 }
 
@@ -85,8 +88,9 @@ function createSuccessRespone($message, $value){
 * Function create Faild-Array
 * Do never create a failed-array this way.
 */
-function createFailResponse($message){
-    $response['code'] = 500;
+function createFailResponse($message) {
+    $response['code']   = 500;
     $response['status'] = $message;
+    logger::log($response['status'], 'warning');
     return $response;
 }
